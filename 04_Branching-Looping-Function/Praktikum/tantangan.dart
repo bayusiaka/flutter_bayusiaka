1b4d48deb0f087ac798a59dc9e3093ed790fb252
Bayu Siaka <bayusiaka7@gmail.com>
2023-08-21 21:35:57 +0800

    Add tantangan

--- 04_Branching-Looping-Function/Praktikum/tantangan.dart
+++ 04_Branching-Looping-Function/Praktikum/tantangan.dart
@@ -7,24 +7,24 @@ void main() {
   var jmlPensil = 1;
   var jmlTas = 1;
 
-  var TotalBarang = TotalBarang(jmlBuku, jmlPensil, jmlTas);
-  var TotalHarga = TotalHarga(hargaBuku, hargaPensil, hargaTas, TotalBarang);
-  var TotalHargaSetelahDiskon = TotalHargaSetelahDiskon(TotalHarga);
+  var TotalBarang = hitungTotalBarang(jmlBuku, jmlPensil, jmlTas);
+  var TotalHarga = hitungTotalHarga(hargaBuku, hargaPensil, hargaTas, TotalBarang);
+  var TotalHargaSetelahDiskon = hitungTotalHargaSetelahDiskon(TotalHarga);
 
   print("Total Barang: $TotalBarang");
   print("Total Harga: $TotalHarga");
   print("Total Harga Setelah Diskon: $TotalHargaSetelahDiskon");
 }
 
-int TotalBarang(int jmlBuku, int jmlPensil, int jmlTas) {
+int hitungTotalBarang(int jmlBuku, int jmlPensil, int jmlTas) {
   return jmlBuku + jmlPensil + jmlTas;
 }
 
-int TotalHarga(int hargaBuku, int hargaPensil, int hargaTas, int TotalBarang) {
+int hitungTotalHarga(int hargaBuku, int hargaPensil, int hargaTas, int TotalBarang) {
   return (hargaBuku * TotalBarang) + (hargaPensil * TotalBarang) + (hargaTas * TotalBarang);
 }
 
-double TotalHargaSetelahDiskon(int TotalHarga) {
+double hitungTotalHargaSetelahDiskon(int TotalHarga) {
   double diskon = 0.10; // 10% diskon
   double potonganDiskon = TotalHarga * diskon;
   return TotalHarga - potonganDiskon;
