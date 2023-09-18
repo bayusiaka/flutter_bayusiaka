bd66ea2663e08b416b3c8ebb9f7c19d68333b133
Bayu Siaka <bayusiaka7@gmail.com>
2023-09-18 16:17:26 +0800

    Add Folder

--- 03_Fundamental-Dart/Praktikum/Tugas.dart
+++ 03_Fundamental-Dart/Praktikum/Tugas.dart
@@ -1,51 +1,51 @@
 // Disclaimer: code ini saya buat terpisah satu sama lain sebelum digabung menjadi satu hehe, thankyouuu
 // Soal Prioritas 1 //
 
-// Soal Nomor 1
-import 'dart:io';
-// Menghitung keliling dan luas persegi
-double hitungKelilingPersegi(double sisi) {
-  return 4 * sisi;
-}
+// // Soal Nomor 1
+// import 'dart:io';
+// // Menghitung keliling dan luas persegi
+// double hitungKelilingPersegi(double sisi) {
+//   return 4 * sisi;
+// }
 
-double hitungLuasPersegi(double sisi) {
-  return sisi * sisi;
-}
+// double hitungLuasPersegi(double sisi) {
+//   return sisi * sisi;
+// }
 
-// Menghitung keliling dan luas persegi panjang
-double hitungKelilingPersegiPanjang(double panjang, double lebar) {
-  return 2 * (panjang + lebar);
-}
+// // Menghitung keliling dan luas persegi panjang
+// double hitungKelilingPersegiPanjang(double panjang, double lebar) {
+//   return 2 * (panjang + lebar);
+// }
 
-double hitungLuasPersegiPanjang(double panjang, double lebar) {
-  return panjang * lebar;
-}
+// double hitungLuasPersegiPanjang(double panjang, double lebar) {
+//   return panjang * lebar;
+// }
 
-void main() {
-  stdout.write("Input Nilai Sisi Persegi: ");
-  double sisiPersegi = double.parse(stdin.readLineSync()!);
+// void main() {
+//   stdout.write("Input Nilai Sisi Persegi: ");
+//   double sisiPersegi = double.parse(stdin.readLineSync()!);
 
-  stdout.write("Input Panjang Persegi Panjang: ");
-  double panjangPersegiPanjang = double.parse(stdin.readLineSync()!);
+//   stdout.write("Input Panjang Persegi Panjang: ");
+//   double panjangPersegiPanjang = double.parse(stdin.readLineSync()!);
 
-  stdout.write("Input Lebar Persegi Panjang: ");
-  double lebarPersegiPanjang = double.parse(stdin.readLineSync()!);
+//   stdout.write("Input Lebar Persegi Panjang: ");
+//   double lebarPersegiPanjang = double.parse(stdin.readLineSync()!);
 
-  double kelilingPersegi = hitungKelilingPersegi(sisiPersegi);
-  double luasPersegi = hitungLuasPersegi(sisiPersegi);
+//   double kelilingPersegi = hitungKelilingPersegi(sisiPersegi);
+//   double luasPersegi = hitungLuasPersegi(sisiPersegi);
 
-  double kelilingPersegiPanjang = hitungKelilingPersegiPanjang(panjangPersegiPanjang, lebarPersegiPanjang);
-  double luasPersegiPanjang = hitungLuasPersegiPanjang(panjangPersegiPanjang, lebarPersegiPanjang);
+//   double kelilingPersegiPanjang = hitungKelilingPersegiPanjang(panjangPersegiPanjang, lebarPersegiPanjang);
+//   double luasPersegiPanjang = hitungLuasPersegiPanjang(panjangPersegiPanjang, lebarPersegiPanjang);
 
-  print("Keliling Persegi: $kelilingPersegi");
-  print("Luas Persegi: $luasPersegi");
+//   print("Keliling Persegi: $kelilingPersegi");
+//   print("Luas Persegi: $luasPersegi");
 
-  print("Keliling Persegi Panjang: $kelilingPersegiPanjang");
-  print("Luas Persegi Panjang: $luasPersegiPanjang");
-}
+//   print("Keliling Persegi Panjang: $kelilingPersegiPanjang");
+//   print("Luas Persegi Panjang: $luasPersegiPanjang");
+// }
 
 
-//Soal Nomor 2
+// //Soal Nomor 2
 import 'dart:io';
 import 'dart:math';
 // Menghitung keliling lingkaran
@@ -53,7 +53,7 @@ double hitungKelilingLingkaran(double radius) {
   return 2 * pi * radius;
 }
 
-// Menghitung luas lingkaran
+Menghitung luas lingkaran
 double hitungLuasLingkaran(double radius) {
   return pi * radius * radius;
 }
@@ -69,89 +69,89 @@ void main() {
   print("Luas Lingkaran: $luas");
 }
 
-// Soal Prioritas 2 //
+// // Soal Prioritas 2 //
 
-// Soal Nomor 1
-import 'dart:io';
+// // Soal Nomor 1
+//import 'dart:io';
 
-void main() {
-  stdout.write("Input Kata Pertama: ");
-  String variabel1 = stdin.readLineSync()!;
+// void main() {
+//   stdout.write("Input Kata Pertama: ");
+//   String variabel1 = stdin.readLineSync()!;
 
-  stdout.write("Input Kata Kedua: ");
-  String variabel2 = stdin.readLineSync()!;
+//   stdout.write("Input Kata Kedua: ");
+//   String variabel2 = stdin.readLineSync()!;
 
-  stdout.write("Input Kata Ketiga: ");
-  String variabel3 = stdin.readLineSync()!;
+//   stdout.write("Input Kata Ketiga: ");
+//   String variabel3 = stdin.readLineSync()!;
 
-  String hasilGabungan = variabel1 + " " + variabel2 + " " + variabel3;
+//   String hasilGabungan = variabel1 + " " + variabel2 + " " + variabel3;
 
-  print("Hasil gabungan: $hasilGabungan");
-}
+//   print("Hasil gabungan: $hasilGabungan");
+// }
 
-// Soal Nomor 2
-import 'dart:io';
-import 'dart:math';
+// // Soal Nomor 2
+// import 'dart:io';
+// import 'dart:math';
 
-void main() {
-  stdout.write("Input Jari-jari Tabung: ");
-  double jariJari = double.parse(stdin.readLineSync()!);
+// void main() {
+//   stdout.write("Input Jari-jari Tabung: ");
+//   double jariJari = double.parse(stdin.readLineSync()!);
 
-  stdout.write("Input Tinggi Tabung: ");
-  double tinggi = double.parse(stdin.readLineSync()!);
+//   stdout.write("Input Tinggi Tabung: ");
+//   double tinggi = double.parse(stdin.readLineSync()!);
 
-  double volume = hitungVolumeTabung(jariJari, tinggi);
+//   double volume = hitungVolumeTabung(jariJari, tinggi);
 
-  print("Volume Tabung: $volume");
-}
+//   print("Volume Tabung: $volume");
+// }
 
-double hitungVolumeTabung(double jariJari, double tinggi) {
-  return pi * jariJari * jariJari * tinggi;
-}
+// double hitungVolumeTabung(double jariJari, double tinggi) {
+//   return pi * jariJari * jariJari * tinggi;
+// }
 
 
-//Soal Eksplorasi
+// //Soal Eksplorasi
 
-//Soal Nomor 1
-import 'dart:io';
+// //Soal Nomor 1
+// import 'dart:io';
 
-void main() {
-  stdout.write("Input Kata: ");
-  String kata = stdin.readLineSync()!.toLowerCase();
+// void main() {
+//   stdout.write("Input Kata: ");
+//   String kata = stdin.readLineSync()!.toLowerCase();
 
-  if (isPalindrome(kata)) {
-    print("Palindrom");
-  } else {
-    print("Bukan Palindrom");
-  }
-}
+//   if (isPalindrome(kata)) {
+//     print("Palindrom");
+//   } else {
+//     print("Bukan Palindrom");
+//   }
+// }
 
-bool isPalindrome(String kata) {
-  int i = 0;
-  int j = kata.length - 1;
+// bool isPalindrome(String kata) {
+//   int i = 0;
+//   int j = kata.length - 1;
 
-  while (i < j) {
-    if (kata[i] != kata[j]) {
-      return false;
-    }
-    i++;
-    j--;
-  }
+//   while (i < j) {
+//     if (kata[i] != kata[j]) {
+//       return false;
+//     }
+//     i++;
+//     j--;
+//   }
 
-  return true;
-}
+//   return true;
+// }
 
-//Soal Nomor 2\
-import 'dart:io';
+// //Soal Nomor 2\
+// import 'dart:io';
 
-void main() {
-  stdout.write("Input Bilangan: ");
-  int bilangan = int.parse(stdin.readLineSync()!);
+// void main() {
+//   stdout.write("Input Bilangan: ");
+//   int bilangan = int.parse(stdin.readLineSync()!);
 
-  print("Faktor dari $bilangan adalah:");
-  for (int i = 1; i <= bilangan; i++) {
-    if (bilangan % i == 0) {
-      print(i);
-    }
-  }
-}
+//   print("Faktor dari $bilangan adalah:");
+//   for (int i = 1; i <= bilangan; i++) {
+//     if (bilangan % i == 0) {
+//       print(i);
+//     }
+//   }
+// }
--- 04_Branching-Looping-Function/Praktikum/Tugas.dart
+++ 04_Branching-Looping-Function/Praktikum/Tugas.dart
@@ -23,146 +23,146 @@ void main() {
   print("Hasil konversi nilai: $hasil");
 }
 
-//Soal Looping (Perulangan)
-void main() {
-  for (int i = 1; i <= 10; i++) {
-    print(i);
-  }
-}
+// //Soal Looping (Perulangan)
+// void main() {
+//   for (int i = 1; i <= 10; i++) {
+//     print(i);
+//   }
+// }
 
 
-// Soal Prioritas 2
+// // Soal Prioritas 2
 
-// Tugas Looping (Perulangan)
-// Nomor 1
-import 'dart:io';
+// // Tugas Looping (Perulangan)
+// // Nomor 1
+// import 'dart:io';
 
-void printn(String text) {
-  stdout.write(text);
-}
+// void printn(String text) {
+//   stdout.write(text);
+// }
 
-void main() {
-  int height = 8;
-  int star = 1;
+// void main() {
+//   int height = 8;
+//   int star = 1;
 
-  for (int i = 1; i <= height; i++) {
-    for (int j = 1; j <= height - i; j++) {
-      printn(" ");
-    }
-    for (int k = 1; k <= star; k++) {
-      printn("*");
-    }
-    star += 2;
-    print("");
-  }
-}
+//   for (int i = 1; i <= height; i++) {
+//     for (int j = 1; j <= height - i; j++) {
+//       printn(" ");
+//     }
+//     for (int k = 1; k <= star; k++) {
+//       printn("*");
+//     }
+//     star += 2;
+//     print("");
+//   }
+// }
 
-//Nomor 2
-import 'dart:io';
+// //Nomor 2
+// import 'dart:io';
 
-void printn(String text) {
-  stdout.write(text);
-}
+// void printn(String text) {
+//   stdout.write(text);
+// }
 
-void main() {
-  int height = 11;
-  // Section Atas
-  for (int i = 1; i <= (height + 1) ~/ 2; i++) {
-    for (int j = 1; j < i; j++) {
-      printn(" ");
-    }
-    for (int k = 1; k <= height - 2 * (i - 1); k++) {
-      printn("0");
-    }
-    print("");
-  }
+// void main() {
+//   int height = 11;
+//   // Section Atas
+//   for (int i = 1; i <= (height + 1) ~/ 2; i++) {
+//     for (int j = 1; j < i; j++) {
+//       printn(" ");
+//     }
+//     for (int k = 1; k <= height - 2 * (i - 1); k++) {
+//       printn("0");
+//     }
+//     print("");
+//   }
 
-  // Section Bawah
-  for (int i = (height + 1) ~/ 2 - 1; i >= 1; i--) {
-    for (int j = i - 1; j >= 1; j--) {
-      printn(" ");
-    }
-    for (int k = height - 2 * (i - 1); k >= 1; k--) {
-      printn("0");
-    }
-    print("");
-  }
-}
-
-
-// Tugas Function (Fungsi)
-// Nomor 1
-import 'dart:math';
-
-double LuasLingkaran(double r) {
-  return pi * r * r;
-}
-
-void main() {
-  double jariLingkaran = 7.0;
-  double luas = LuasLingkaran(jariLingkaran);
-  print("Luas lingkaran dengan jari-jari (r) = $jariLingkaran adalah $luas");
-}
-
-// Nomor 2
-void main() {
-  List<int> values = [10, 40, 50];
-
-  for (int value in values) {
-    int factor = 1;
-    for (int i = 1; i <= value; i++) {
-      factor *= i;
-    }
-    print("Faktorial $value adalah $factor");
-  }
-}
+//   // Section Bawah
+//   for (int i = (height + 1) ~/ 2 - 1; i >= 1; i--) {
+//     for (int j = i - 1; j >= 1; j--) {
+//       printn(" ");
+//     }
+//     for (int k = height - 2 * (i - 1); k >= 1; k--) {
+//       printn("0");
+//     }
+//     print("");
+//   }
+// }
 
 
-// Soal Eksplorasi
-// Nomor 1
+// // Tugas Function (Fungsi)
+// // Nomor 1
+// import 'dart:math';
 
-import 'dart:io';
+// double LuasLingkaran(double r) {
+//   return pi * r * r;
+// }
 
-void main() {
-  stdout.write("Input sebuah bilangan: ");
-  int number = int.parse(stdin.readLineSync()!);
+// void main() {
+//   double jariLingkaran = 7.0;
+//   double luas = LuasLingkaran(jariLingkaran);
+//   print("Luas lingkaran dengan jari-jari (r) = $jariLingkaran adalah $luas");
+// }
 
-  if (isPrime(number)) {
-    print("$number adalah bilangan prima.");
-  } else {
-    print("$number bukan bilangan prima.");
-  }
-}
+// // Nomor 2
+// void main() {
+//   List<int> values = [10, 40, 50];
 
-bool isPrime(int num) {
-  if (num <= 1) {
-    return false;
-  }
+//   for (int value in values) {
+//     int factor = 1;
+//     for (int i = 1; i <= value; i++) {
+//       factor *= i;
+//     }
+//     print("Faktorial $value adalah $factor");
+//   }
+// }
+
+
+// // Soal Eksplorasi
+// // Nomor 1
+
+// import 'dart:io';
+
+// void main() {
+//   stdout.write("Input sebuah bilangan: ");
+//   int number = int.parse(stdin.readLineSync()!);
+
+//   if (isPrime(number)) {
+//     print("$number adalah bilangan prima.");
+//   } else {
+//     print("$number bukan bilangan prima.");
+//   }
+// }
+
+// bool isPrime(int num) {
+//   if (num <= 1) {
+//     return false;
+//   }
   
-  for (int i = 2; i <= num / 2; i++) {
-    if (num % i == 0) {
-      return false;
-    }
-  }
+//   for (int i = 2; i <= num / 2; i++) {
+//     if (num % i == 0) {
+//       return false;
+//     }
+//   }
   
-  return true;
-}
+//   return true;
+// }
 
-// Nomor 2
-import 'dart:io';
+// // Nomor 2
+// import 'dart:io';
 
-void main() {
-  stdout.write("Input angka untuk tabel perkalian: ");
-  int number = int.parse(stdin.readLineSync()!);
-  printTable(number);
-}
+// void main() {
+//   stdout.write("Input angka untuk tabel perkalian: ");
+//   int number = int.parse(stdin.readLineSync()!);
+//   printTable(number);
+// }
 
-void printTable(int n) {
-  for (int i = 1; i <= n; i++) {
-    for (int j = 1; j <= n; j++) {
-      stdout.write((i * j).toString().padLeft(4)); // Menggunakan padLeft untuk adjusting lebar kolom
-    }
-    print(""); // Pindah ke baris baru setelah setiap baris selesai
-  }
-}
+// void printTable(int n) {
+//   for (int i = 1; i <= n; i++) {
+//     for (int j = 1; j <= n; j++) {
+//       stdout.write((i * j).toString().padLeft(4)); // Menggunakan padLeft untuk adjusting lebar kolom
+//     }
+//     print(""); // Pindah ke baris baru setelah setiap baris selesai
+//   }
+// }
 
